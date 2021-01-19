3242be33e687fad73063e5ffec54f705673c1a2c
Nicklaus <18210855790@163.com>
2021-01-19 16:38:08 +0800

    0.0.1

---
+++ PushSDK.framework/Headers/PushSDK.h
@@ -0,0 +1,19 @@
+//
+//  PushSDK.h
+//  PushSDK
+//
+//  Created by edz on 2021/1/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PushSDK.
+FOUNDATION_EXPORT double PushSDKVersionNumber;
+
+//! Project version string for PushSDK.
+FOUNDATION_EXPORT const unsigned char PushSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PushSDK/PublicHeader.h>
+
+#import <PushSDK/Test.h>
+
---
+++ PushSDK.framework/Headers/Test.h
@@ -0,0 +1,18 @@
+//
+//  Test.h
+//  PushSDK
+//
+//  Created by edz on 2021/1/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
++ (void)setHostUrl:(NSString *)str;
+
+@end
+
+NS_ASSUME_NONNULL_END
